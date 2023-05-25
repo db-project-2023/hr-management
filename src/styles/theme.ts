74382874894fc57cc7c9fd159d858bbb85276c86
quadiez <dduidokdoi@gmail.com>
2023-05-25 09:45:26 +0700

    feat: setup font

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,10 @@
 import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
+    fonts: {
+        heading: "Inter",
+        body: "Inter"
+    },
     colors: {
         brand: {
             primary: '#E8804C',
